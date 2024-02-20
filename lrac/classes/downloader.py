a988198f8d6c8f137d946cc2148aae9601b3ed26
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-02-20 12:02:42 -0600

    Add additional functionality for the downloader class

--- lrac/classes/downloader.py
+++ lrac/classes/downloader.py
@@ -5,23 +5,27 @@ from requests import Response, get
 
 class PDFDownloader:
     def __init__(self) -> None:
-        pass
+        self.currentFilepath: Path | None = None
+        self.currentResponse: Response | None = None
+        self.currentURL: str | None = None
 
-    def download(self, url: str, doi: str, storageDirectory: Path) -> Path | None:
+    def clear(self) -> None:
+        self.currentFilepath = None
+        self.currentResponse = None
+        self.currentURL = None
+
+    def download(self, url: str, doi: str, storageDirectory: Path) -> None:
         """
         Downloads a PDF and saves it to a specific storageDirectory with the
         file pattern 'doi.pdf'.
 
         Forward slashes are replaced with $ signs for safety.
         """
-        filepath: Path = Path(storageDirectory, f"{doi}.pdf")
+        self.currentURL = url
+        self.currentFilepath = Path(storageDirectory, f"{doi}.pdf")
+        self.currentResponse = get(url=self.currentURL)
 
-        resp: Response = get(url=url)
-
-        if resp.status_code == 200:
-            with open(file=filepath, mode="wb") as pdfFile:
-                pdfFile.write(resp.content)
+        if self.currentResponse.status_code == 200:
+            with open(file=self.currentFilepath, mode="wb") as pdfFile:
+                pdfFile.write(self.currentResponse.content)
                 pdfFile.close()
-            return filepath
-
-        return None
