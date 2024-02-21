0ce698169fe6b3c7624282f78fedf03182ac3a66
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-02-21 13:49:22 -0600

    Add support for identifying if a class uses a protocol over an ABC

--- lrac/utils/metaprogramming.py
+++ lrac/utils/metaprogramming.py
@@ -1,12 +1,27 @@
 import inspect
 from abc import ABCMeta
 from types import ModuleType
-from typing import List
+from typing import Any, List, Protocol
 
 
-def findSubclasses(module: ModuleType, abc: ABCMeta) -> List[ABCMeta]:
+def findSubclasses(module: ModuleType, protocol: Any) -> List[Any]:
+    """
+    This method can identify if a class in a module adhears to a defined
+    Protocol class.
+
+    It returns a list of instantied classes that do adhear to the Protocol.
+    """
     subclasses = []
     for _, obj in inspect.getmembers(module):
-        if inspect.isclass(obj) and issubclass(obj, abc) and obj != abc:
-            subclasses.append(obj)
+        if inspect.isclass(obj) and obj != protocol:
+            try:
+                tempObj: Any = obj()
+            except TypeError:
+                continue
+            else:
+                if isinstance(tempObj, protocol):
+                    subclasses.append(tempObj)
+                else:
+                    del tempObj
+
     return subclasses
