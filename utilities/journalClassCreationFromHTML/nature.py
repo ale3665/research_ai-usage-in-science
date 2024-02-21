84f58050cb03d8deae633eec39e8ac59852011b3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-02-21 17:37:42 -0600

    Write code to generate classes of Nature journals from HTML

--- utilities/journalClassCreationFromHTML/nature.py
+++ utilities/journalClassCreationFromHTML/nature.py
@@ -1,6 +1,5 @@
 from os.path import isfile
 from pathlib import Path
-from typing import List
 
 from bs4 import BeautifulSoup, ResultSet, Tag
 from requests import Response, get
@@ -36,13 +35,46 @@ def extractRelevantData(soup: BeautifulSoup) -> dict[str, str]:
     return data
 
 
+def buildClass(name: str, link: str) -> str:
+    className: str = (
+        name.replace(" ", "")
+        .replace("&", "And")
+        .replace("-", "")
+        .replace(":", "")
+        .replace("'", "")
+        .replace(r"\xe2\x80\x94", "")
+    )
+    url: str = NATURE_BASE_URL + link
+    feedType: str = "rss"
+    feedURL: str = f"{url[:-1]}.rss"
+
+    return f"""
+class {className}:
+    def __init__(self)  ->  None:
+        self.name = "{name}"
+        self.url = "{url}"
+        self.feedType = "{feedType}"
+        self.feedURL = "{feedURL}"
+        self.entryTags = []
+        self.entryTagKeys = []
+        self.endtryDownloadURLTemplate = ""
+
+    def entryDownloadURL(self, **kwargs)    ->  str:
+        ...
+"""
+
+
 def main() -> None:
     soup: BeautifulSoup = getRawNaturePortfolio()
     nameHREFPairs: dict[str, str] = extractRelevantData(soup=soup)
 
-    from pprint import pprint as print
+    data: str = ""
 
-    print(nameHREFPairs)
+    key: str
+    for key in nameHREFPairs.keys():
+        data += buildClass(name=key, link=nameHREFPairs[key])
+
+    print(data)
 
 
 if __name__ == "__main__":
