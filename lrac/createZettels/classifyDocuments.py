5f0496409dec12b328a2e1151699b77b5f03b538
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-25 10:30:48 -0500

    Add code for getting the model output in JSON format

--- lrac/createZettels/classifyDocuments.py
+++ lrac/createZettels/classifyDocuments.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 import click
 import pandas
 import sqlalchemy
 from langchain_community.llms.ollama import Ollama
+from langchain_core.exceptions import OutputParserException
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables.base import RunnableSequence
@@ -30,7 +31,8 @@ def readDB(dbPath: Path) -> DataFrame:
     return zettels
 
 
-def inference(llm: Ollama, prompt: str) -> dict:
+def inference(llm: Ollama, pairing: Tuple[str, Path]) -> Tuple[Path, str]:
+    prompt: str = pairing[0]
     outputParser: JsonOutputParser = JsonOutputParser()
     chatPrompt: ChatPromptTemplate = ChatPromptTemplate.from_messages(
         [
@@ -41,7 +43,20 @@ def inference(llm: Ollama, prompt: str) -> dict:
 
     chain: RunnableSequence = chatPrompt | llm | outputParser
 
-    return chain.invoke(input={"input": prompt})
+    classification: str
+    try:
+        response: dict = chain.invoke({"input": prompt})
+        classification = (
+            llm.model.lower()
+            + ":"
+            + list(response.values())[0].replace(" ", "_").lower()
+        )
+    except OutputParserException:
+        classification = "llm_erorr"
+    except AttributeError:
+        classification = "llm_erorr"
+
+    return (pairing[1], classification)
 
 
 @click.command()
@@ -69,14 +84,19 @@ def main(inputDB: Path, model: str) -> None:
     df: DataFrame = readDB(dbPath=dbPath)
     relevantDF: DataFrame = df[["c0title", "c8note", "c13filename"]]
 
-    data: List[str] = [" ".join(i) for i in zip(df["c0title"], df["c8note"])]
+    data: List[str] = [
+        " ".join(i) for i in zip(relevantDF["c0title"], relevantDF["c8note"])
+    ]
+    dataPathPairings: List[Tuple[str, Path]] = [
+        (i[0], Path(dbPathDir, i[1])) for i in zip(data, relevantDF["c13filename"])
+    ]
 
     llm: Ollama = Ollama(model=model)
 
     with Bar("Classifying data based on title and abstract...", max=len(data)) as bar:
-        datum: str
-        for datum in data:
-            inference(llm=llm, prompt=datum)
+        pair: Tuple[str, Path]
+        for pair in dataPathPairings:
+            print(inference(llm=llm, pairing=pair))
             bar.next()
 
 
