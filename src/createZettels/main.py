c8ba53c8415b49e2a5ab1a5410e3c8d4fab984b9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-24 19:11:08 -0500

    Start work on adding functionality

--- src/createZettels/main.py
+++ src/createZettels/main.py
@@ -3,17 +3,52 @@ from pathlib import Path
 import click
 import pandas
 from pandas import DataFrame
-from pyfs import isFile, resolvePath
+from pyfs import isDirectory, isFile, resolvePath
+
+from src.downloadPapers import Journal_ABC
+from src.downloadPapers.nature import Nature
+from src.downloadPapers.plos import PLOS
+from src.downloadPapers.science import Science
 
 
-def main() -> None:
-    inputPath: Path = Path("../../data/plos/parquet/papers.parquet")
-
+@click.command()
+@click.option(
+    "-i",
+    "--input",
+    "inputPath",
+    type=Path,
+    required=True,
+    help="Path to a journal's paper parquet file",
+)
+@click.option(
+    "-o",
+    "--output",
+    "outputDir",
+    type=Path,
+    required=True,
+    help="Directory to save Zettels to",
+)
+def main(inputPath: Path, outputDir: Path) -> None:
     absInputPath: Path = resolvePath(path=inputPath)
+    absOutputDirPath: Path = resolvePath(path=outputDir)
 
     assert isFile(path=absInputPath)
+    assert isDirectory(path=absOutputDirPath)
 
     df: DataFrame = pandas.read_parquet(path=absInputPath, engine="pyarrow")
+    journalName: str = df["journal"][0]
+
+    journal: Journal_ABC
+    match journalName:
+        case "Nature":
+            journal = Nature()
+        case "PLOS":
+            journal = PLOS()
+        # case "Science":
+        #     journal = Science()
+        case _:
+            print("Unsupported journal")
+            exit(1)
 
 
 if __name__ == "__main__":
