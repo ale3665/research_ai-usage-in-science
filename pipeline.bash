b9de73eac20585931f320bf8453175c3caee135f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-08 15:40:19 -0500

    Delete code to get science papers

--- pipeline.bash
+++ pipeline.bash
@@ -4,8 +4,8 @@ source optparse.bash
 # Search through journals flags
 optparse.define short=a long=nature-search-result-output desc="Path to save Nature search results to" variable=natureSearchResultOutput
 optparse.define short=b long=plos-search-result-output desc="Path to save PLOS search results to" variable=plosSearchResultOutput
-optparse.define short=b long=plos-search-result-output desc="Path to save PLOS search results to" variable=plosSearchResultOutput
-optparse.define short=c long=science-search-result-output desc="Path to save Science search results to" variable=scienceSearchResultOutput
+
+
 
 source $( optparse.build )
 
@@ -17,4 +17,5 @@ ssro=$(realpath $scienceSearchResultOutput)
 # Search through journals
 aius-search -j nature -o $nsro
 aius-search -j plos -o $psro
-aius-search -j science -o $ssro
+
+# Download papers
