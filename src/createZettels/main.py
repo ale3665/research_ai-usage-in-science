902ad497c5433025e7576e41b6aff2343554298b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-09 07:55:28 -0500

    Start copying over content from previous Zettel wrapper

--- src/createZettels/main.py
+++ src/createZettels/main.py
@@ -1,4 +1,8 @@
+import re
+from collections import namedtuple
 from pathlib import Path
+from subprocess import PIPE, Popen  # nosec
+from tempfile import NamedTemporaryFile
 from typing import List
 
 import click
@@ -12,6 +16,55 @@ from pyfs import isDirectory, isFile, resolvePath
 # from src.journals.nature import Nature
 
 # from zettelgeist.zettel import Zettel
+
+ZETTEL = namedtuple(
+    typename="zettel",
+    field_names=[
+        "doi",
+        "title",
+        "abstract",
+        "document",
+        "tags",
+        "path",
+    ],
+)
+
+
+def formatText(string: str) -> str:
+    string = re.sub(pattern=r"-\n", repl="", string=string)
+    string = string.replace("\n", "")
+    string = " ".join(string.split())
+    return string
+
+
+def runZettel(zettel: ZETTEL) -> bool:
+    summaryTemp: NamedTemporaryFile = NamedTemporaryFile(
+        mode="w+t", delete=False
+    )
+    noteTemp: NamedTemporaryFile = NamedTemporaryFile(mode="w+t", delete=False)
+
+    summaryTemp.write(zettel.abstract)
+    noteTemp.write(zettel.document)
+
+    summaryTemp.close()
+    noteTemp.close()
+
+    url: str = f"https://doi.org/{zettel.doi.replace('_', '/')}"
+    cmd: str = (
+        f'zettel --set-title "{zettel.title}" \
+                --set-url {url} \
+                --load-summary {summaryTemp.name} \
+                --load-note {noteTemp.name} \
+                --append-tags {" ".join(zettel.tags).strip()} \
+                --save "{zettel.path}"'
+    )
+
+    process: Popen[bytes] = Popen(cmd, shell=True, stdout=PIPE)  # nosec
+
+    if process.returncode == 0:
+        return True
+    else:
+        return False
 
 
 def extractPLOSContent(df: DataFrame) -> DataFrame:
@@ -33,6 +86,9 @@ def extractPLOSContent(df: DataFrame) -> DataFrame:
                 name="div", attrs={"class": "abstract-content"}
             )
             abstract: str = abstractContainer.findChild(name="p").text
+
+            title = formatText(string=title)
+            abstract = formatText(string=abstract)
 
             data["title"].append(title)
             data["summary"].append(abstract)
@@ -74,6 +130,8 @@ def main(inputPath: Path, outputDir: Path) -> None:
     if not isDirectory(path=absOutputDirPath):
         print(f"{absOutputDirPath} is not a directory")
         exit(1)
+
+    # data: List[ZETTEL] = []
 
     print(f"Reading {absInputPath} ...")
     df: DataFrame = pandas.read_parquet(path=absInputPath, engine="pyarrow")
