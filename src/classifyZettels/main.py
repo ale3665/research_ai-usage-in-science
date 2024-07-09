1207250345d3fa8779aca23700fa40705da6d865
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-09 11:16:16 -0500

    Start work on building LLM inferencing with lang chain

--- src/classifyZettels/main.py
+++ src/classifyZettels/main.py
@@ -1,5 +1,35 @@
+# from typing import List
+
+from langchain_community.llms.ollama import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables.base import RunnableSequence
+
+# from src.classifyZettels import NATURE_SUBJECTS
+
+
+def buildRunnableSequence(model: str = "llama3") -> RunnableSequence:
+
+    llm: Ollama = Ollama(model=model)
+
+    prompt: ChatPromptTemplate = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a world class technical documentation writer.",
+            ),
+            ("user", "{input}"),
+        ]
+    )
+
+    output: StrOutputParser = StrOutputParser()
+
+    return prompt | llm | output
+
+
 def main() -> None:
-    pass
+    chain: RunnableSequence = buildRunnableSequence()
+    print(type(chain))
 
 
 if __name__ == "__main__":
