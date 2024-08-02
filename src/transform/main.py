50af3fb08108707aa80c94c100e4934a3c2155b7
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-02 14:59:48 -0500

    Extract data from html content

--- src/transform/main.py
+++ src/transform/main.py
@@ -3,8 +3,46 @@ from typing import List
 
 import click
 import pandas
-from pandas import DataFrame
+from bs4 import BeautifulSoup
+from pandas import DataFrame, Series
+from progress.bar import Bar
 from pyfs import resolvePath
+
+from src.classes.journalGeneric import Journal_ABC
+from src.classes.plos import PLOS
+
+
+def extractContent(df: DataFrame, journal: Journal_ABC) -> DataFrame:
+    data: dict[str, List[str]] = {
+        "doi": [],
+        "url": [],
+        "titles": [],
+        "abstracts": [],
+        "content": [],
+        "tags": [],
+    }
+
+    with Bar("Extracting content from HTML...", max=df.shape[0]) as bar:
+        row: Series
+        for _, row in df.iterrows():
+            data["doi"].append(row["doi"])
+            data["url"].append(row["url"])
+
+            soup: BeautifulSoup = BeautifulSoup(
+                markup=row["html"],
+                features="lxml",
+            )
+
+            data["titles"].append(journal.extractTitleFromPaper(soup=soup))
+            data["abstracts"].append(
+                journal.extractAbstractFromPaper(soup=soup)
+            )
+            data["content"].append(journal.extractContentFromPaper(soup=soup))
+            data["tags"].append(journal.extractJournalTagsFromPaper(soup=soup))
+
+            bar.next()
+
+    return DataFrame(data=data)
 
 
 @click.command()
@@ -27,20 +65,15 @@ from pyfs import resolvePath
 def main(inputPath: Path, outputPath: Path) -> None:
     absInputPath: Path = resolvePath(path=inputPath)
     absOutputPath: Path = resolvePath(path=outputPath)
-    print(absOutputPath)
-
-    data: dict[str, List[str]] = {
-        "url": [],
-        "titles": [],
-        "abstracts": [],
-        "content": [],
-        "tags": [],
-    }
 
     df: DataFrame = pandas.read_parquet(path=absInputPath)
 
-    print(df)
-    print(data)
+    foo: DataFrame = df[df["status_code"] == 200]
+
+    extractContent(df=foo, journal=PLOS()).to_parquet(
+        path=absOutputPath,
+        engine="pyarrow",
+    )
 
 
 if __name__ == "__main__":
