cc0f3d350fc03fe50cff9618ae7b673c09edf20c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-08 11:25:22 -0500

    Add plot tool

--- src/run.bash
+++ src/run.bash
@@ -4,4 +4,8 @@ DATE=$(date +"%m-%d-%Y")
 # Step 1: Search for documents within mega journals
 aius-search --journal plos --output ../data/plos/search_$DATE.parquet
 
+# Step 2: Plot search result statistics
+aius-search-plot --input ../data/plos/search_$DATE.parquet --fig-1 ../data/plos/plos_searchResultPagesPerYear.png --fig-2 ../data/plos/plos_searchResultPagesPerQuery.png
+
+
 # Step 2: Filter for papers indexed in OpenAlex
