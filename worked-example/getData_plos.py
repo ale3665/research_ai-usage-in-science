9464deb6d6cdc8799abf1476de444fc523779bf3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-08-12 10:41:15 -0500

    Save data to JSON file across multiple pages

--- worked-example/getData_plos.py
+++ worked-example/getData_plos.py
@@ -1,62 +1,52 @@
-from json import dump, load
-from math import ceil
 from os.path import isfile
 from typing import List
 
+import pandas
 from pandas import DataFrame
+from progress.bar import Bar
 from requests import Response, get
 
+RANDOM_SEED: int = 42
 
-def getJSONResponse() -> dict:
+
+def getJSONResponse() -> DataFrame:
     filename: str = "searchResponse_plos.json"
 
-    json: dict
+    json: DataFrame
     if isfile(path=filename):
-        with open(file="searchResponse_plos.json", mode="r") as jsonFile:
-            json = load(fp=jsonFile)
-            jsonFile.close()
+        json = pandas.read_json(path_or_buf=filename)
+
     else:
-        # Query: "Deep Learning"
-        # Year: 2016
-        # Page: 1
-        url: str = (
-            "https://journals.plos.org/plosone/dynamicSearch?filterStartDate=2016-01-01&filterEndDate=2016-12-31&resultsPerPage=100&q=%E2%80%9DDeep%20Learning%E2%80%9D&sortOrder=DATE_NEWEST_FIRST&page=74&filterArticleTypes=Research%20Article"  # noqa: E501
-        )
+        dfs: List[DataFrame] = []
 
-        resp: Response = get(url=url, timeout=60)
-        json = resp.json()
+        with Bar("Getting JSON responses...", max=75) as bar:
+            page: int
+            for page in range(1, 76):
+                # Query: "Deep Learning"
+                # Year: 2016
+                # Page: 1
+                # Max page: 75
+                url: str = (
+                    f"https://journals.plos.org/plosone/dynamicSearch?filterStartDate=2016-01-01&filterEndDate=2016-12-31&resultsPerPage=100&q=%E2%80%9DDeep%20Learning%E2%80%9D&sortOrder=DATE_NEWEST_FIRST&page={page}&filterArticleTypes=Research%20Article"  # noqa: E501
+                )
 
-        with open(file="searchResponse_plos.json", mode="w") as jsonFile:
-            dump(obj=json, fp=jsonFile)
-            jsonFile.close()
+                resp: Response = get(url=url, timeout=60)
+                json = resp.json()
+
+                dfs.append(DataFrame(data=json["searchResults"]["docs"]))
+
+                bar.next()
+
+        json = pandas.concat(objs=dfs, ignore_index=True)
+        json["id"] = json["id"].apply(lambda x: f"https://doi.org/{x}")
+        json.to_json(path_or_buf=filename, index=False, indent=4)
 
     return json
 
 
-def searchResultStats(searchResults: dict[str, dict | List]) -> None:
-    print("Total documents found:", searchResults["numFound"])
-    print("Documents per page:", len(searchResults["docs"]))
-    print("Number of pages:", ceil(searchResults["numFound"] / 100))
-
-
-def sampleDocs(docs: List[dict[str, str | dict]]) -> DataFrame:
-    df: DataFrame = DataFrame(data=docs)
-    df["id"] = df["id"].apply(lambda x: f"https://doi.org/{x}")
-
-    return df.sample(n=30, replace=False, random_state=42, ignore_index=True)
-
-
 def main() -> None:
-    json: dict = getJSONResponse()
-
-    searchResults: dict[str, dict | List] = json["searchResults"]
-    searchResultStats(searchResults=searchResults)
-
-    docs: List[dict[str, str]] = searchResults["docs"]
-
-    sampleDF: DataFrame = sampleDocs(docs=docs)
-
-    print(sampleDF)
+    json: DataFrame = getJSONResponse()
+    print("Total documents found:", json.shape[0])
 
 
 if __name__ == "__main__":
